d129f73d0bcbb72888cde675b866b38a764fcc67
Lokesh Ranganathan <lokesh.ranganathan@spotcap.com>
2021-05-19 20:40:50 +0200

    custom input has been re-wired with existing app

--- src/buyflow/Buyflow.tsx
+++ src/buyflow/Buyflow.tsx
@@ -1,42 +1,84 @@
-import React, { useState } from 'react'
-import AgeStep from './AgeStep'
-import EmailStep from './EmailStep'
-import SummaryStep from './SummaryStep'
+import React, {useState} from "react";
+import SummaryStep from "./SummaryStep";
+import CustomInput from "../components/CustomInput";
+import {
+    isNotEmpty,
+    isNumberLessThan,
+    isNumberGreaterThan,
+    isValidEmail
+} from "../utils/index";
 
 interface BuyflowProps {
-  productId: ProductIds
+    productId: ProductIds;
 }
 
 export enum ProductIds {
-  devIns = 'dev_ins',
+    devIns = "dev_ins"
 }
 
 const PRODUCT_IDS_TO_NAMES = {
-  [ProductIds.devIns]: 'Developer Insurance',
-}
+    [ProductIds.devIns]: "Developer Insurance"
+};
+
+const initialState = {
+    email: "",
+    age: 0,
+    firstname: "",
+    lastname: ""
+};
 
 const Buyflow: React.FC<BuyflowProps> = (props) => {
-  const [currentStep, setStep] = useState('email')
-  const [collectedData, updateData] = useState({
-    email: '',
-    age: 0,
-  })
-  const getStepCallback = (nextStep: string) => (field: string, value: any) => {
-    updateData({ ...collectedData, [field]: value })
-    setStep(nextStep)
-  }
-  return (
-    <>
-      <h4>Buying {PRODUCT_IDS_TO_NAMES[props.productId]}</h4>
-      {(currentStep === 'email' && <EmailStep cb={getStepCallback('age')} />) ||
-        (currentStep === 'age' && (
-          <AgeStep cb={getStepCallback('summary')} />
-        )) ||
-        (currentStep === 'summary' && (
-          <SummaryStep collectedData={collectedData} />
-        ))}
-    </>
-  )
-}
+    const [currentStep, setStep] = useState("firstname");
+    const [collectedData, updateData] = useState(initialState);
+    const getStepCallback = (nextStep: string) => (field: string, value: any) => {
+        updateData({...collectedData, [field]: value});
+        setStep(nextStep);
+    };
+    return (
+        <>
+            <h4>Buying {PRODUCT_IDS_TO_NAMES[props.productId]}</h4>
 
-export default Buyflow
+            {(currentStep === "firstname" && (
+                <CustomInput
+                    label="First Name"
+                    name="firstname"
+                    type="text"
+                    isValidField={isNotEmpty}
+                    handler={getStepCallback("lastname")}
+                />
+            )) ||
+            (currentStep === "lastname" && (
+                <CustomInput
+                    label="Last Name"
+                    name="lastname"
+                    type="text"
+                    isValidField={isNotEmpty}
+                    handler={getStepCallback("email")}
+                />
+            )) ||
+            (currentStep === "email" && (
+                <CustomInput
+                    label="Email"
+                    name="email"
+                    type="text"
+                    isValidField={isValidEmail}
+                    handler={getStepCallback("age")}
+                />
+            )) ||
+            (currentStep === "age" && (
+                <CustomInput
+                    label="Age"
+                    name="age"
+                    type="number"
+                    isValidField={[isNumberGreaterThan(0), isNumberLessThan(125)]}
+                    handler={getStepCallback("summary")}
+                />
+            )) ||
+            (currentStep === "summary" && (
+                <SummaryStep collectedData={collectedData}/>
+            ))}
+        </>
+    );
+};
+
+export default Buyflow;
